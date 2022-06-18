a67511cacc3c17b7dbc1cdd9d00e1e25318aebf7
aom31 <97218117+aom31@users.noreply.github.com>
2022-06-19 02:36:12 +0700

    create

---
+++ securityauthen/src/main/java/com/example/securityauthen/SecurityAuthenApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityauthen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAuthenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAuthenApplication.class, args);
+	}
+
+}
---
+++ securityauthen/src/test/java/com/example/securityauthen/SecurityAuthenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityauthen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAuthenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
