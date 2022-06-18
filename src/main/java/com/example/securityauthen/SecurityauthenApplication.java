c4a64a8df49548614e716b69f34f315e4bf2a86b
aom31 <97218117+aom31@users.noreply.github.com>
2022-06-19 02:38:17 +0700

    create

--- src/main/java/com/example/securityauthen/SecurityauthenApplication.java
+++ src/main/java/com/example/securityauthen/SecurityauthenApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecurityAuthenApplication {
+public class SecurityauthenApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecurityAuthenApplication.class, args);
+		SpringApplication.run(SecurityauthenApplication.class, args);
 	}
 
 }
